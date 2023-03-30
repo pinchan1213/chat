ec09f50870924ffb5d1245b2149f4b3343dda6fb
pinchan1213 <hello.tra.fujino@gmail.com>
2023-03-30 16:41:59 +0900

    サーバーコミット

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,8 @@ class AppServiceProvider extends ServiceProvider
         // $this->app->make('Edit::class');
             // if (App::environment('production','staging')) {
             URL::forceScheme('https');
+
+        //   aaaaaaaaaa
             // }
     }
 }
